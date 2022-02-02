3855fc38d60a4dcda674e2b07c4ab61f690ff17d
Americo <americo@mercury.com>
2022-02-02 16:31:15 -0700

    Clean up install script

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Get the current dotfiles and copy them to a back-up folder
-install_dir=`pwd`
-cd ~
+install_dir=$(pwd)
+cd "$HOME" || exit
 mkdir bak
 find . .* -type f -maxdepth 0 -exec cp {} ./bak \;
-cd install_dir
+cd "$install_dir" || exit
 find . .* -type f -maxdepth 0 -exec ln -sf {} ~ \;
 cp ./fonts/* ~/Library/Fonts
 cp ./com.googlecode.iterm2.plist ~/Library/Preferences/
